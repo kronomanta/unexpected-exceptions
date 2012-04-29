4823659c7d666fd53a38aad4ca80d78c5080d784
kanyo.tibi@gmail.com <kanyo.tibi@gmail.com@be5db142-ef1c-fdd5-07a8-748fafb7d050>
2012-04-29 08:22:58 +0000

    Javított indítófájlok ;)

--- trunk/Source/Final/build.bat
+++ trunk/Source/Final/build.bat
@@ -2,11 +2,11 @@
 if not exist bin (mkdir bin)
 
 echo Compiling classes...
-javac src/main/*.java src/model/*.java src/gameLogic/*.java src/engine/*.java src/game/*.java src/game/renderer/*.java src/game/level/*.java -d bin -encoding UTF-8
+javac src/main/*.java src/model/*.java src/gameLogic/*.java src/game/*.java src/game/renderer/*.java src/game/level/*.java -d bin -encoding UTF-8
 
 cd bin
 echo Creating jar...
-jar cfm ../Continuity.jar ../src/manifest.txt main/*.class model/*.class engine/*.class game/*.class gameLogic/*.class game/renderer/*.class game/level/*.class
+jar cfm ../Continuity.jar ../src/manifest.txt main/*.class model/*.class game/*.class gameLogic/*.class game/renderer/*.class game/level/*.class
 cd ..
 
 echo Done
