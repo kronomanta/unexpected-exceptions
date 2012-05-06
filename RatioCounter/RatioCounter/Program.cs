5dc4446116a41ccc55302bcaa34cdee6ce79754a
kanyo.tibi@gmail.com <kanyo.tibi@gmail.com@be5db142-ef1c-fdd5-07a8-748fafb7d050>
2012-05-06 08:29:14 +0000

    ADD: forrássort számoló progi, és beírva az értékek a doksira

---
+++ RatioCounter/RatioCounter/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace RatioCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+            var ratio = new Dictionary<string, double>
+                            {
+                                {"Vajsz", 0},
+                                {"Kanyó", 0},
+                                {"Biró", 0},
+                                {"Tarjáni", 0},
+                                {"Magyar", 0}
+                            };
+
+            try
+            {
+                var exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+                
+                var texFiles = Directory.GetFiles(exePath, @"*.tex", SearchOption.TopDirectoryOnly)
+                    .Where(a=> Path.GetFileName(a).StartsWith("0") || Path.GetFileName(a).StartsWith("1"))
+                    .ToArray();
+
+                foreach (var texFile in texFiles)
+                {
+                    var file = File.ReadAllText(texFile);
+                    file = file.Replace("\n","");
+                    int journalIdx = file.IndexOf("{journal}");
+                    file = file.Substring(journalIdx+9);
+                    var journalEntries = file.Split(new[]{@"\journalentry"},30,StringSplitOptions.RemoveEmptyEntries);
+                    if(journalEntries.Length>0)
+                    {
+                        foreach (var journalEntry in journalEntries)
+                        {
+                            var entry = journalEntry.Split(new[] { "}{" }, 4, StringSplitOptions.None);
+                            var hour = Double.Parse(entry[1].Replace(',','.'),CultureInfo.InvariantCulture);
+                            var names = entry[2].Split(' ');
+
+                            foreach (string name in names)
+                            {
+                                if (ratio.ContainsKey(name.Replace(',', ' ')))
+                                {
+                                    ratio[name] += hour;
+                                }
+                            }  
+                        }
+                    }
+                }
+
+                Console.WriteLine("Num of tex files: "+texFiles.Length);
+                var sum = ratio.Values.Sum();
+                foreach (KeyValuePair<string, double> pair in ratio)
+                {
+                    Console.WriteLine("{0}: {1} --> {2:0.0}%", pair.Key, pair.Value, pair.Value*100/sum);
+                }
+                Console.WriteLine("Total worktime: {0} hour(s)", sum);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: "+ex);
+            }
+        }
+    }
+}
---
+++ Source/Utilities/SourceLinesCounter/SourceLinesCounter/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using SourceLinesCounter.Properties;
+
+namespace SourceLinesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skeletonLines = 0, protoLines = 0, finalLines = 0;
+
+            Console.WriteLine("Searching for java files in {0}", Settings.Default.SkeletonDirectoryPath);
+            Console.WriteLine();
+
+            if(!Directory.Exists(Settings.Default.SkeletonDirectoryPath)){
+                Console.WriteLine("Directory does not exist...");
+            }else
+            {
+                string[] filePaths = Directory.GetFiles(Settings.Default.SkeletonDirectoryPath, "*.java", SearchOption.AllDirectories);
+                foreach (var filePath in filePaths)
+                {
+                    Console.WriteLine("Processing {0}", Path.GetFileName(filePath));
+                    string[] fileContent = File.ReadAllLines(filePath);
+
+                    skeletonLines += fileContent.Where(IsSourceLine).Count();
+                }    
+            }
+            
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Searching for java files in {0}", Settings.Default.PrototypeDirectoryPath);
+            Console.WriteLine();
+
+            if (!Directory.Exists(Settings.Default.PrototypeDirectoryPath))
+            {
+                Console.WriteLine("Directory does not exist...");
+            }else
+            {
+                string[] filePaths = Directory.GetFiles(Settings.Default.PrototypeDirectoryPath, "*.java", SearchOption.AllDirectories);
+                foreach (var filePath in filePaths)
+                {
+                    Console.WriteLine("Processing {0}", Path.GetFileName(filePath));
+                    string[] fileContent = File.ReadAllLines(filePath);
+
+                    protoLines += fileContent.Where(IsSourceLine).Count();
+                }    
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Searching for java files in {0}", Settings.Default.FinalDirectoryPath);
+            Console.WriteLine();
+
+
+            if (!Directory.Exists(Settings.Default.PrototypeDirectoryPath))
+            {
+                Console.WriteLine("Directory does not exist...");
+            }else
+            {
+                string[] filePaths = Directory.GetFiles(Settings.Default.FinalDirectoryPath, "*.java", SearchOption.AllDirectories);
+                foreach (var filePath in filePaths)
+                {
+                    Console.WriteLine("Processing {0}", Path.GetFileName(filePath));
+                    string[] fileContent = File.ReadAllLines(filePath);
+
+                    finalLines += fileContent.Where(IsSourceLine).Count();
+                }
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Lines of Code:");
+            Console.WriteLine("Skeleton: {0}", skeletonLines);
+            Console.WriteLine("Prototype: {0}", protoLines);
+            Console.WriteLine("Final: {0}", finalLines);
+            Console.ReadKey();
+        }
+
+        private static bool IsSourceLine(string str)
+        {
+            str = str.TrimStart(' ');
+            return (!str.StartsWith("/*") && !str.StartsWith("* ") && !str.StartsWith("//") && str.Length>0);
+        }
+    }
+}
